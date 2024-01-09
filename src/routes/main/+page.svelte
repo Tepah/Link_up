fab2b64dcf345d288957c720d2fc37d678a1690b
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2024-01-09 00:32:22 -0800

    Implemented tailwind

---
+++ src/routes/main/+page.svelte
@@ -0,0 +1,15 @@
+
+<div>
+    <h1>Line Up</h1>
+</div>
+<div>
+    <p>Upcoming Plans</p>
+    <!-- TODO: Add a list of upcoming plans -->
+</div>
+<div>
+    <p>Current Plans</p>
+    <!-- TODO: Add a list of current plans -->
+</div>
+<div>
+    <button>New Plan</button>
+</div>
---
+++ src/routes/scheduler/[plan]/+page.svelte
@@ -0,0 +1,8 @@
+
+
+<div>
+    <p>Choose your availability:</p>
+    <div>
+        <!-- TODO: add calendar here -->
+    </div>
+</div>
