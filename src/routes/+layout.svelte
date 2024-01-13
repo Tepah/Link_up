516029b5c90d0a7965c80a692c172e10f4c94a54
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2024-01-13 14:47:41 -0800

    Added a few animations

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,7 +4,7 @@
 
 
 <div class={"flex items-center justify-center h-screen bg-background"}>
-    <div class="flex-shrink flex flex-col self-start md:self-center text-center sm:h-screen md:h-[63%] overflow-auto m-10">
+    <div class="flex-shrink flex flex-col self-start md:self-center text-center sm:h-screen md:h-[63%] overflow-auto px-6 m-5">
         <slot />
     </div>
 </div>
--- src/routes/main/+page.svelte
+++ src/routes/main/+page.svelte
@@ -1,5 +1,6 @@
 <script>
     import titleLogo from "$lib/images/main_title.png";
+    import settingsIcon from "$lib/images/icons8-lines.svg";
     export let data;
 
     // Using this to create a toggle for a list to show all upcoming plans. Will use for animation.
@@ -13,143 +14,134 @@
       showAllIncomplete = !showAllIncomplete;
     }
 </script>
-
-<div class="flex-shrink flex flex-col">
-    <div class="flex items-center justify-center p-1">
-        <img src="{titleLogo}" alt="title logo">
+<div>
+    <div class="relative">
+        <button class="absolute accent-accent top-2 -right-5 p-2">
+            <img src={settingsIcon} alt="settings" class="h-8">
+        </button>
     </div>
-    <div class="p-1">
-        <p>Hi, <span class="text-accent"> name</span>!</p>
-    </div>
-</div>
-{#if !showAllIncomplete}
-<div class="flex-shrink flex flex-col p-1.5">
-    <div class="flex flex-row">
-        <p class="text-start text-lg">Upcoming Plans</p>
-        {#if showAllUpcoming && data.upcoming.length > 3}
-            <button class=" font-bold flex items-center justify-center p-1" on:click={toggleUpcoming}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                </svg>
-            </button>
-        {:else if !showAllUpcoming && data.upcoming.length > 3}
-            <button class=" font-bold flex items-center justify-center p-1" on:click={toggleUpcoming}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                </svg>
-            </button>
-        {/if}
-    </div>
-    {#if data.upcoming.length === 0}
-        <div class="my-3">
-            <p class="text-sm">Nothing yet...</p>
+    <div class="flex-shrink flex flex-col">
+        <div class="flex items-center justify-center p-1">
+            <img src="{titleLogo}" alt="title logo">
         </div>
-    {:else if !showAllUpcoming}
-        <div class="flex flex-col my-3">
-            {#each data.upcoming.slice(0, 3) as plan}
-                <div class="flex flex-row justify-between w-72 h-20 py-3 pl-3 pr-4 my-1.5 bg-primary bg-opacity-50 rounded-xl">
-                    <div class="flex flex-col items-start pl-1 pr-7">
-                        <p class="text-lg font-semibold">{plan.title}</p>
-                        <p class="">{plan.attending.length} going</p>
-                    </div>
-                    <div class="flex p-2 items-start">
-                        <p class="text-lg">{plan.date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}</p>
-                    </div>
-                </div>
-            {/each}
-            {#if data.upcoming.length > 3}
-                <button on:click={toggleUpcoming} class="text-lg font-bold flex flex-row items-center justify-center">...</button>
-            {/if}
+        <div class="p-1">
+            <p>Hi, <span class="text-accent"> name</span>!</p>
         </div>
-    {:else}
-        <div class="flex flex-col my-3 h-96">
-            {#each data.upcoming as plan}
-                <div class="flex flex-row justify-between w-72 h-20 py-3 pl-3 pr-4 my-1.5 bg-primary bg-opacity-50 rounded-xl">
-                    <div class="flex flex-col items-start pl-1 pr-7">
-                        <p class="text-lg font-semibold">{plan.title}</p>
-                        <p class="">{plan.attending.length} going</p>
-                    </div>
-                    <div class="flex p-2 items-start">
-                        <p class="text-lg">{plan.date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}</p>
-                    </div>
-                </div>
-            {/each}
-            <button on:click={toggleUpcoming} class="text-lg font-bold flex flex-row items-center justify-center"></button>
-        </div>
-    {/if}
-</div>
-{/if}
-{#if !showAllUpcoming || data.incomplete.length < 0}
-<div class="flex-shrink flex flex-col p-1.5">
-    <div class="flex flex-row">
-        <p class="text-start text-lg">Undecided Plans</p>
-        {#if showAllIncomplete && data.incomplete.length > 2}
-            <button class=" font-bold flex items-center justify-center p-1" on:click={toggleUndecided}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m19.5 8.25-7.5 7.5-7.5-7.5" />
-                </svg>
-            </button>
-        {:else if !showAllIncomplete && data.incomplete.length > 2}
-            <button class=" font-bold flex items-center justify-center p-1" on:click={toggleUndecided}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
-                    <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
-                </svg>
-            </button>
-        {/if}
     </div>
     {#if !showAllIncomplete}
-        {#if data.incomplete.length !== 0}
+    <div class="flex-shrink flex flex-col p-1.5">
+        <div class="flex flex-row">
+            <p class="text-start text-lg">Upcoming Plans</p>
+            {#if data.upcoming.length > 3}
+                <button class="font-bold flex items-center justify-center p-1" class:show-list={showAllUpcoming} class:hide-list={!showAllUpcoming} on:click={toggleUpcoming}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    </svg>
+                </button>
+            {/if}
+        </div>
+        {#if data.upcoming.length === 0}
+            <div class="my-3">
+                <p class="text-sm">Nothing yet...</p>
+            </div>
+        {:else if !showAllUpcoming}
             <div class="flex flex-col my-3">
-                {#each data.incomplete.slice(0, 2) as plan}
-                    <div class="flex flex-row justify-between h-20 w-72 py-3 pl-3 pr-4 my-1.5 bg-secondary bg-opacity-20 rounded-xl">
+                {#each data.upcoming.slice(0, 3) as plan}
+                    <div class="planItem flex flex-row justify-between w-72 h-20 py-3 pl-3 pr-4 my-1.5 bg-primary bg-opacity-50 rounded-xl">
+                        <div class="flex flex-col items-start pl-1 pr-7">
+                            <p class="text-lg font-semibold">{plan.title}</p>
+                            <p class="">{plan.attending.length} going</p>
+                        </div>
+                        <div class="flex p-2 items-start">
+                            <p class="text-lg">{plan.date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}</p>
+                        </div>
+                    </div>
+                {/each}
+                {#if data.upcoming.length > 3}
+                    <button on:click={toggleUpcoming} class="text-lg font-bold flex flex-row items-center justify-center">...</button>
+                {/if}
+            </div>
+        {:else}
+            <div class="flex flex-col my-3 h-96">
+                {#each data.upcoming as plan}
+                    <div class="planItem flex flex-row justify-between w-72 h-20 py-3 pl-3 pr-4 my-1.5 bg-primary bg-opacity-50 rounded-xl">
+                        <div class="flex flex-col items-start pl-1 pr-7">
+                            <p class="text-lg font-semibold">{plan.title}</p>
+                            <p class="">{plan.attending.length} going</p>
+                        </div>
+                        <div class="flex p-2 items-start">
+                            <p class="text-lg">{plan.date.toLocaleDateString('en-US', { month: 'short', day: 'numeric' })}</p>
+                        </div>
+                    </div>
+                {/each}
+                <button on:click={toggleUpcoming} class="text-lg font-bold flex flex-row items-center justify-center"></button>
+            </div>
+        {/if}
+    </div>
+    {/if}
+    {#if !showAllUpcoming || data.incomplete.length < 0}
+    <div class="flex-shrink flex flex-col p-1.5">
+        <div class="flex flex-row">
+            <p class="text-start text-lg">Undecided Plans</p>
+            {#if data.incomplete.length > 2}
+                <button class="font-bold flex items-center justify-center p-1" class:show-list={showAllIncomplete} class:hide-list={!showAllIncomplete} on:click={toggleUndecided}>
+                    <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-4 h-4">
+                        <path stroke-linecap="round" stroke-linejoin="round" d="m8.25 4.5 7.5 7.5-7.5 7.5" />
+                    </svg>
+                </button>
+            {/if}
+        </div>
+        {#if !showAllIncomplete}
+            {#if data.incomplete.length !== 0}
+                <div class="flex flex-col my-3">
+                    {#each data.incomplete.slice(0, 2) as plan}
+                        <div class="planItem flex flex-row justify-between h-20 w-72 py-3 pl-3 pr-4 my-1.5 bg-secondary bg-opacity-20 rounded-xl">
+                            <div class="flex flex-col items-start pl-1 pr-7">
+                                <p class="text-lg font-semibold">{plan.title}</p>
+                                <p class=""><span class="text-accent">{plan.schedules.length}</span> schedules</p>
+                            </div>
+                        </div>
+                    {/each}
+                    {#if data.incomplete.length > 2}
+                        <button class="flex flex-row items-center justify-center text-lg font-bold" on:click={toggleUndecided}>...</button>
+                    {/if}
+                </div>
+            {/if}
+        {:else}
+            <div class="flex flex-col my-3">
+                {#each data.incomplete as plan}
+                    <div class="planItem flex flex-row justify-between h-20 w-72 py-3 pl-3 pr-4 my-1.5 bg-secondary bg-opacity-20 rounded-xl">
                         <div class="flex flex-col items-start pl-1 pr-7">
                             <p class="text-lg font-semibold">{plan.title}</p>
                             <p class=""><span class="text-accent">{plan.schedules.length}</span> schedules</p>
                         </div>
                     </div>
                 {/each}
-                {#if data.incomplete.length > 2}
-                    <button class="flex flex-row items-center justify-center text-lg font-bold" on:click={toggleUndecided}>...</button>
-                {/if}
             </div>
         {/if}
-    {:else}
-        <div class="flex flex-col my-3">
-            {#each data.incomplete as plan}
-                <div class="flex flex-row justify-between h-20 w-72 py-3 pl-3 pr-4 my-1.5 bg-secondary bg-opacity-20 rounded-xl">
-                    <div class="flex flex-col items-start pl-1 pr-7">
-                        <p class="text-lg font-semibold">{plan.title}</p>
-                        <p class=""><span class="text-accent">{plan.schedules.length}</span> schedules</p>
-                    </div>
-                </div>
-            {/each}
-        </div>
+    </div>
+    <div class="p-2">
+        <button class="planItem py-2 px-8 bg-primary text-lg rounded-xl">New Plan</button>
+    </div>
     {/if}
 </div>
-<div class="p-5">
-    <button class="py-2 px-8 bg-primary text-lg rounded-xl">New Plan</button>
-</div>
-{/if}
 
 <style>
-    .list-container {
-        display: none;
+    .planItem {
+        transition: transform 0.2s ease;
     }
 
-    .list-container.show {
-        display: block;
+    .planItem:hover {
+        transform: scale(1.1);
     }
 
-    @keyframes fadeIn {
-        from {
-            opacity: 0;
-        }
-        to {
-            opacity: 1;
-        }
+    .show-list {
+        transition: transform 0.2s ease;
+        transform: rotate(90deg);
     }
 
-    .animate__fadeIn {
-        animation: fadeIn 0.5s ease;
+    .hide-list {
+        transition: transform 0.2s ease;
+        transform: rotate(0deg);
     }
 </style>
