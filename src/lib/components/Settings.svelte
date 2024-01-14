8d79ac935aecbb0ad744823f4e0f1d81c74f23a6
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2024-01-14 12:26:51 -0800

    Creating layout for settings

---
+++ src/lib/components/Settings.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+    export let toggleSettings: () => void;
+    import profilePic from "$lib/images/profile.jpeg";
+</script>
+
+<div class="flex flex-col justify-center items-center w-[100%]">
+    <div>
+        <h1 class="text-2xl font-semibold">Settings</h1>
+    </div>
+    <div>
+        <img src={profilePic} alt="profile" class="rounded-full w-24 h-24"/>
+    </div>
+    <div>
+        <p>Dark Mode</p>
+        <p>Change name</p>
+        <p>Change email</p>
+        <p>Change phone number</p>
+        <p>Change password</p>
+        <p>Sign out</p>
+    </div>
+    <div class="flex flex-row justify-evenly p-2 w-[90%]">
+        <button class="planItem py-1 px-6 bg-primary text-lg rounded-xl" on:click={toggleSettings}>Close</button>
+    </div>
+</div>
--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,8 +3,8 @@
 </script>
 
 
-<div class={"flex items-center justify-center h-screen bg-background"}>
-    <div class="flex-shrink flex flex-col self-start md:self-center text-center sm:h-screen md:h-[63%] overflow-auto px-6 m-5">
+<div class={"flex items-center justify-center h-screen bg-background overflow-auto"}>
+    <div class="flex-shrink flex flex-col content-center self-start md:self-center text-center sm:h-screen md:h-[63%] w-80 overflow-auto px-6 m-5">
         <slot />
     </div>
 </div>
