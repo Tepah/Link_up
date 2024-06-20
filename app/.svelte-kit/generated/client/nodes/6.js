1e03a9e65662f2d1f7df8cf8bce45a44da7a7762
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2024-06-19 21:42:45 -0700

    Created express, mongodb, node backend with a User Schema

---
+++ app/.svelte-kit/generated/client/nodes/6.js
@@ -0,0 +1,1 @@
+export { default as component } from "../../../../src/routes/confirm/[plan]/schedules/+page.svelte";
---
+++ server/src/index.ts
@@ -0,0 +1,25 @@
+import express from 'express';
+import mongoose from 'mongoose';
+import userRoutes from './routes/userRoutes';
+
+require("dotenv").config();
+
+const app = express();
+const PORT = 3000
+
+// @ts-ignore
+mongoose.connect(process.env.MONGODB_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch(error => {
+    console.error('Error connecting to MongoDB', error);
+})
+
+app.use(express.json());
+app.use(userRoutes);
+
+app.listen(PORT, () => {
+    console.log('Server is running on http://localhost:3000')
+})
