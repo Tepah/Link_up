187d4bb5052b17a0d53bd8ff5ab6a83841a71e11
Pete Potipitak <67202848+Tepah@users.noreply.github.com>
2024-08-03 13:14:14 -0700

    Created Page without styling, will add styles later

--- server/src/controllers/userController.ts
+++ server/src/controllers/userController.ts
@@ -1,4 +1,5 @@
 import { Request, Response } from 'express';
+import bcrypt from 'bcrypt';
 import { User } from '../models/User';
 
 export const getUsers = async (req: Request, res: Response) => {
@@ -12,8 +13,28 @@ export const getUsers = async (req: Request, res: Response) => {
 
 export const getUser = async ( req: Request, res: Response) => {
     try {
-        const user = await User.findById(req.params.id);
+        const user = await User.findById(req.params.id, '-password');
         res.json(user);
+    } catch (error) {
+        res.status(500).send(error);
+    }
+}
+
+export const loginUser = async (req: Request, res: Response) => {
+    try {
+        const { email, password } = req.body;
+
+        const user = await User.findOne({ email });
+        if (user) {
+            const valid = await bcrypt.compare(password, user.password);
+            if (valid) {
+                res.status(200).json(user);
+            } else {
+                res.status(401).send('Invalid credentials');
+            }
+        } else {
+            return res.status(404).send('User not found');
+        }
     } catch (error) {
         res.status(500).send(error);
     }
@@ -21,11 +42,15 @@ export const getUser = async ( req: Request, res: Response) => {
 
 export const createUser = async (req: Request, res: Response) => {
     try {
-        const user = new User(req.body);
+        const { username, name, email, password } = req.body;
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, name, email, password: hashedPassword });
+
         await user.save();
-        res.status(201).json(user);
+        res.status(200).json(user);
     } catch (error) {
-        res.status(400).send(error);
+        res.status(500).send(error);
     }
 }
 
--- server/src/routes/userRoutes.ts
+++ server/src/routes/userRoutes.ts
@@ -1,11 +1,12 @@
 import { Router } from 'express';
-import { getUsers, getUser, createUser, updateUser, deleteUser } from '../controllers/userController';
+import {getUsers, getUser, createUser, updateUser, deleteUser, loginUser} from '../controllers/userController';
 
 const router = Router();
 
 router.get('/users', getUsers);
 router.get('/users/:id', getUser);
 router.post('/users', createUser);
+router.post('/users/login', loginUser);
 router.put('/users/:id', updateUser)
 router.delete('/users/:id', deleteUser);
 
